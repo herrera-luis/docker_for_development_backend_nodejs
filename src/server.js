d66c25ef4a67f9a406c33ada9dce3d5e94f0af52
Cristian Toaquiza <cristoaquizal@gmail.com>
2018-05-21 17:37:19 -0500

    adds to acept headers from client

--- src/server.js
+++ src/server.js
@@ -5,6 +5,12 @@ const app = express();
 const httpServer = require('http').createServer(app);
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 app.use('/todos', apiController);
 
 module.exports = httpServer;
